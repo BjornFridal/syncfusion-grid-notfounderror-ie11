b9621d0d1dc68127f971a6c2a229de636e9da5f0
Bjørn Fridal <bjorn@fridal.dk>
2021-05-05 23:31:36 +0200

    Bug example

--- src/App.js
+++ src/App.js
@@ -1,25 +1,74 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import {
+  GridComponent,
+  ColumnsDirective,
+  ColumnDirective
+} from '@syncfusion/ej2-react-grids';
+import '../node_modules/@syncfusion/ej2-base/styles/material.css';
+import '../node_modules/@syncfusion/ej2-react-grids/styles/material.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      showGrid: true
+    };
+    this.toggleShowGrid = this.toggleShowGrid.bind(this);
+  }
+
+  toggleShowGrid() {
+    this.setState({ showGrid: !this.state.showGrid });
+  }
+
+  render() {
+    return (
+      <div>
+        <button onClick={this.toggleShowGrid}>toggle grid</button>
+        {this.state.showGrid && <Grid />}
+      </div>
+    );
+  }
 }
 
-export default App;
+class Grid extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      data: [
+        {
+          id: 1,
+          name: 'Orange'
+        },
+        {
+          id: 2,
+          name: 'Azure'
+        },
+        {
+          id: 3,
+          name: 'Pink'
+        },
+        {
+          id: 4,
+          name: 'Lime'
+        }
+      ]
+    };
+  }
+
+  template = this.gridTemplate;
+
+  gridTemplate(props) {
+    return <div>{props.name}</div>;
+  }
+
+  render() {
+    return (
+      <GridComponent dataSource={this.state.data}>
+        <ColumnsDirective>
+          <ColumnDirective headerText="ID" field="id" />
+          <ColumnDirective headerText="Name" template={this.template} />
+        </ColumnsDirective>
+      </GridComponent>
+    );
+  }
+}
